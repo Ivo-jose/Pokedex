565c4f67196b5215f8f68304ee819c361f76d6d6
Ivo Goncalves <ivojmgs19@gmail.com>
2023-09-22 11:00:18 -0300

    Creation of modules for Lazy Loading

--- pokedex/src/app/app.module.ts
+++ pokedex/src/app/app.module.ts
@@ -1,7 +1,10 @@
+//Modules
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
+import { PagesModule } from './pages/pages.module';
+
+//Componets
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -10,7 +13,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    PagesModule
   ],
   providers: [],
   bootstrap: [AppComponent]
