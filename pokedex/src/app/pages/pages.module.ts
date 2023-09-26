9f9ad087a012525442130c32e0e632dfeed035e9
Ivo Goncalves <ivojmgs19@gmail.com>
2023-09-26 10:28:45 -0300

    Creating components and fitting them into the application

--- pokedex/src/app/pages/pages.module.ts
+++ pokedex/src/app/pages/pages.module.ts
@@ -2,10 +2,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RoutingModule } from './routing.module';
+import { SharedModule } from '../shared/shared.module';
 
 //Components
 import { HomeComponent } from './home/home.component';
 import { DetailsComponent } from './details/details.component';
+
 
 
 
@@ -16,7 +18,8 @@ import { DetailsComponent } from './details/details.component';
   ],
   imports: [
     CommonModule,
-    RoutingModule
+    RoutingModule,
+    SharedModule
   ]
 })
 export class PagesModule { }
