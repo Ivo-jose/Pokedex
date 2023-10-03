ec0df5aec69c077b807b3f5c9ec801f57fbd921a
Ivo Goncalves <ivojmgs19@gmail.com>
2023-10-03 11:00:22 -0300

    Fetching specific data for the Details component

--- pokedex/src/app/pages/details/details.component.ts
+++ pokedex/src/app/pages/details/details.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { forkJoin } from 'rxjs';
 
+// Service
+import { PokeApiService } from 'src/app/service/poke-api.service';
 @Component({
   selector: 'app-details',
   templateUrl: './details.component.html',
   styleUrls: ['./details.component.scss']
 })
-export class DetailsComponent {
+export class DetailsComponent implements OnInit {
 
+  private urlPokemon: string = 'https://pokeapi.co/api/v2/pokemon';
+  private urlName: string = 'https://pokeapi.co/api/v2/pokemon-species';
+
+  constructor(
+    private activedRouter: ActivatedRoute,
+    private pokeApiService: PokeApiService
+  ){}
+
+  ngOnInit(): void {
+    this.pokemon;
+  }
+
+  get pokemon() {
+    const id = this.activedRouter.snapshot.params['id'];
+    const pokemon = this.pokeApiService.apiGetPokemons(`${this.urlPokemon}/${id}`);
+    const name = this.pokeApiService.apiGetPokemons(`${this.urlName}/${id}`);
+    return forkJoin([pokemon,name]).subscribe({
+      next: res => console.log(res),
+      error: err => console.log(err)
+    });
+  }
 }
